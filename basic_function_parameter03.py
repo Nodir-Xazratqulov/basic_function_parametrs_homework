d54a5b66d8730120e03b235b51f7ccc7acbb4512
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-13 10:54:08 +0500

    func-3

--- basic_function_parameter03.py
+++ basic_function_parameter03.py
@@ -1,3 +1,9 @@
 # Create a function named find_smallest that takes a list of numbers as a parameter.
 # Inside the function, find the smallest number in the given list.
 # Return the smallest number.
+
+
+def find(a):
+    return min(a)
+print(find(a=[5, 2, 9, 1, 7]))
+
