59151e52c8517ce1aca490f852117b034860ad0a
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-13 10:54:47 +0500

    func-4

--- basic_function_parameter04.py
+++ basic_function_parameter04.py
@@ -2,3 +2,11 @@
 # Inside the function, calculate the average of all the numbers in the given list.
 # Return the average.
 # Return the average.
+
+
+def find(a):
+    s=0
+    for i in a:
+        s+=i
+    return int(s/len(a))
+print(find(a=[85, 90, 92, 88, 95]))
